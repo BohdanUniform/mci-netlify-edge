633bb43f1b444dbe65119fb7c3801da9b6d34979
Bohdan Zatirko <bohdan@Bohdans-MacBook-Pro.local>
2023-03-23 13:37:40 +0100

    trying default

--- netlify/edge-functions/context-middleware.ts
+++ netlify/edge-functions/context-middleware.ts
@@ -23,8 +23,6 @@ export default async (request: Request, netlifyContext: Context) => {
     ...buildNetlifyQuirks(netlifyContext),
     ...(await getCDPData(netlifyContext)),
   };
-  console.log("quirks are gonna set here");
-
   console.log({ quirks });
   const { processed, response } = await handler({
     context,
@@ -49,22 +47,22 @@ export default async (request: Request, netlifyContext: Context) => {
 };
 
 async function getCDPData(netlifyContext: Context) {
-  
-  const CUSTOMER_INSIGHTS_ENDPOINT = process.env.CUSTOMER_INSIGHTS_ENDPOINT;
-  const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-  const CUSTOMER_INSIGHTS_INSTANCE = process.env.CUSTOMER_INSIGHTS_INSTANCE; //42d1b1fa-046f-487a-ad62-b9d99b09ecba
+  const ajs_anonymous_id = netlifyContext.cookies.get("ajs_anonymous_id");
+  console.log({ visitorId: ajs_anonymous_id });
+  if (!ajs_anonymous_id) {
+    return {};
+  }
 
-  //const {  customerId }= "85d0a2193825204e3cabe79f0ec4bb95"; 
+  // @ts-ignore: deno imports failing next build
+  const segmentSpaceId = Deno.env.get("SEGMENT_SPACE_ID");
+  // @ts-ignore: deno imports failing next build
+  const segmentBasicAuth = Deno.env.get("SEGMENT_BASIC_AUTH");
 
-
-  const url = "https://api.ci.ai.dynamics.com/v1/instances/42d1b1fa-046f-487a-ad62-b9d99b09ecba/data/Customer?$filter=CustomerId eq '3c65adba444e75db32a78180cdb295ba'";
+  const url = `https://profiles.segment.com/v1/spaces/${segmentSpaceId}/collections/users/profiles/anonymous_id:${ajs_anonymous_id}/traits`;
 
   const visitorResponse = await fetch(url, {
     headers: {
-      'Cache-Control':'no-cache',
-      'Ocp-Apim-Subscription-Key': '9d13571d1c444381942738c05eaac76d',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiIwYmZjNDU2OC1hNGJhLTRjNTgtYmQzZS01ZDNlNzZiZDdmZmYiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85YzMxYzhlMy04MzEwLTQ1YjUtOGRiZC04ZmE3OTIwN2ExOTAvIiwiaWF0IjoxNjc5NTY3NjAwLCJuYmYiOjE2Nzk1Njc2MDAsImV4cCI6MTY3OTU3Mjk0MywiYWNyIjoiMSIsImFpbyI6IkFUUUF5LzhUQUFBQWdRWkR4cUJ4NzFBKzJKRUN5TlJkYzR3VUZSQ0wwZ0x4RWl5Y1JqbEwybEZzUlJ0a0pMUlRZQi9TS29OQnJicUkiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiODE4MTc2YTQtNDQ1YS00YmZiLTlhZjMtNmY4MzY0ZTkxZGYwIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJaYXRpcmtvIiwiZ2l2ZW5fbmFtZSI6IkJvaGRhbiIsImdyb3VwcyI6WyJmYzQzNzBjNS00MzFkLTRkMTgtYTczNy1hZTk3ZDM2YjIxYjQiXSwiaXBhZGRyIjoiODguMTMwLjIwNS44MiIsIm5hbWUiOiJCb2hkYW4gWmF0aXJrbyIsIm9pZCI6IjhhZDk2OTcwLTA5OWItNDI1Mi04ZTc0LTVjNmQ4MWExNmQ1MiIsInB1aWQiOiIxMDAzMjAwMURFNkYxMDJFIiwicmgiOiIwLkFWSUE0OGd4bkJDRHRVV052WS1ua2dlaGtHaEZfQXU2cEZoTXZUNWRQbmE5Zl85U0FQZy4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJuckxySWxVZ0IxTVJzelJpTG9vR0F6dUhaOTZFcmVqb0tVRU9zOGhuRV9RIiwidGlkIjoiOWMzMWM4ZTMtODMxMC00NWI1LThkYmQtOGZhNzkyMDdhMTkwIiwidW5pcXVlX25hbWUiOiJib2hkYW56QHVuaWZvcm0uZGV2IiwidXBuIjoiYm9oZGFuekB1bmlmb3JtLmRldiIsInV0aSI6Imk0QlVYeGxzYjBtMDVkVkhaR2tiQUEiLCJ2ZXIiOiIxLjAifQ.sKWNubM6urd_RzlEY-FchFiEjR31PehYC65R20DZsi2gh-UPKUszAwmp51epdf1G8kV7OlT3jQA6xYjX-dGUEkfc4z1Aqs0EiVqr7Qy_QtBza8PZc9FG9UO36dy-GaB-AppWHNr2kEuSSed1I4sopqQV01Dx3ODD8Wseq4prIJPfhxqWp5yWpIxQ9vlHmN_bPPATw29clnfBwJzMZmHi1wz3lO-CQ01t9a4Y9UWhWFTy6bbaVn2GTmKE9bkg2A5C5O4F6Z60s2OedpZZJXdU3TPU6BC2113qEercoYWyHfWS5kYDoMVORBnrTpOXnXaWEw9vTsFgK-ihyu022ExouA`,
+      Authorization: `Basic ${segmentBasicAuth}`,
     },
   });
   console.log({ visitorResponse });
@@ -73,45 +71,9 @@ async function getCDPData(netlifyContext: Context) {
     return {};
   }
   const visitorData = await visitorResponse.json();
-  console.log(visitorData);
-  
   const traits = removeUnderscores(visitorData?.traits);
   console.log({ traits });
-
-
-
-
-
-  console.log("Its getting here"); 
-  const ajs_anonymous_id = netlifyContext.cookies.get("ajs_anonymous_id");
-  console.log("visitorId");
-
-  console.log({ visitorId: ajs_anonymous_id });
-  if (!ajs_anonymous_id) {
-    return {};
-  }
-
-  // // @ts-ignore: deno imports failing next build
-  // const segmentSpaceId = Deno.env.get("SEGMENT_SPACE_ID");
-  // // @ts-ignore: deno imports failing next build
-  // const segmentBasicAuth = Deno.env.get("SEGMENT_BASIC_AUTH");
-
-  // const url = `https://profiles.segment.com/v1/spaces/${segmentSpaceId}/collections/users/profiles/anonymous_id:${ajs_anonymous_id}/traits`;
-
-  // const visitorResponse = await fetch(url, {
-  //   headers: {
-  //     Authorization: `Basic ${segmentBasicAuth}`,
-  //   },
-  // });
-  // console.log({ visitorResponse });
-  // if (!visitorResponse.ok) {
-  //   console.log("Error fetching CDP data");
-  //   return {};
-  // }
-  // const visitorData = await visitorResponse.json();
-  // const traits = removeUnderscores(visitorData?.traits);
-  // console.log({ traits });
-  // return traits;
+  return traits;
 }
 
 function shouldProcess(request: Request) {
